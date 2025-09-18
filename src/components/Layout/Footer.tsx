44f7b36501bf87b1c01b4209c212990b9e273a61
Isaac <exessgamer@gmail.com>
2025-09-18 16:24:59 -0600

    fixing z-index in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="p-4 text-center text-sm w-full h-[var(--footer-height)] bg-[var(--color-primary)] text-white z-0">
+    <footer className=" p-4 text-center text-sm w-full h-[var(--footer-height)] bg-[var(--color-primary)] text-white z-50">
       © {new Date().getFullYear()} Mi Empresa. Todos los derechos reservados.
     </footer>
   );
